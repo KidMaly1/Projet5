40e4d519c09bb03191ddb09b99c987959b97e087
Andria T Rama <titouslef@gmail.com>
2023-12-07 16:37:14 +0100

    first commit Projet5

---
+++ assets/script.js
@@ -0,0 +1,101 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+const nbslides = slides.length;
+console.log (nbslides)
+const flechedroite = document.querySelector('.arrow_right');
+const flechegauche = document.querySelector('.arrow_left');
+let number = 0;
+
+function slidesuivant() {
+	
+	number++;
+	if (number > nbslides - 1) {
+		number = 0
+	}
+	
+	console.log(number)
+
+src="./assets/images/slideshow/"+slides[number]["image"]
+	console.log(src)
+	const image = document.querySelector('.banner-img');
+	image.src = src
+	console.log(image.src)
+}
+
+
+flechedroite.addEventListener('click', slidesuivant)
+
+function slideprecedent() {
+	
+number--;
+if (number < 0) {
+	number = nbslides - 1
+}
+
+
+	src="./assets/images/slideshow/"+slides[number]["image"]
+	console.log(src)
+	const image = document.querySelector('.banner-img');
+	image.src = src
+	console.log(image.src)
+
+	}
+
+	flechegauche.addEventListener('click', slideprecedent)
+
+
+dots = document.querySelector('.dots');
+
+
+for (let compteur = 0; compteur < nbslides; compteur = compteur + 1) {
+
+    let dot = document.createElement("div");
+
+dot.classList.add("dot") 
+dot.setAttribute("data-index", compteur);
+
+dots.appendChild(dot);
+
+dot.addEventListener('click', slidedot)
+
+}
+
+function slidedot () {
+	console.log("jai cliqué ici")
+	console.log(this)
+
+console.log(this.getAttribute("data-index"))
+	
+	
+
+	number = this.getAttribute("data-index")
+
+
+
+	src="./assets/images/slideshow/"+slides[number]["image"]
+	console.log(src)
+	const image = document.querySelector('.banner-img');
+	image.src = src
+	console.log(image.src)
+	
+
+}
+
+
